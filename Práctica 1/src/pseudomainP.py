bf685adbe075f081fee059d314614da99176f7dc
Daniel Alexander Cabañas Gonzalez <daniel.cabannas@estudiante.uam.es>
2020-03-01 16:49:31 +0100

    avanzamos, modos añadidos y test parcialmente funcional

--- Práctica 1/src/pseudomainP.py
+++ Práctica 1/src/pseudomainP.py
@@ -3,13 +3,54 @@ import numpy as np
 from Neurona import *
 from Enlace import *
 from sklearn.model_selection import train_test_split
+import math
+import sys
+import random
+
+# Lectura del fichero
+
+if len(sys.argv) < 5:
+    print("introduzca la cantidad de parámetros correcta, para mas información utilice el makefile")
+
+modo = 0
+
+if len(sys.argv[2]) > 3:
+
+    # Modo 3
+    modo = 3
+    print("Modo 3")
+elif int(sys.argv[2]) == 100:
+
+    # Modo 2
+    modo = 2
+    print("Modo 2")
+else:
+
+    # Modo 1
+    modo = 1
+    print("Modo 1")
+
+if modo == 1 or modo == 2:
+    print("Fichero de datos: " + sys.argv[1])
+    print("Porcentaje de train: " + sys.argv[2])
+else:
+    print("Fichero de datos de train: " + sys.argv[1])
+    print("Fichero de datos de test: " + sys.argv[2])
+print("Umbral: " + sys.argv[3])
+print("Tasa de aprendizaje: " + sys.argv[4])
+print("Número máximo de épocas: " + sys.argv[5])
 
 # cosas que entrarán por argumento
-fichero = "problema_real1.txt"
-umbral = 0.2
-porcen_train = 0.7
-a = 0.3  # tasa de aprendizaje
-maxEpocas = 100
+fichero = sys.argv[1]
+
+if modo == 1:
+    porcen_train = int(sys.argv[2])
+elif modo == 3:
+    fichero2 = sys.argv[2]
+
+umbral = float(sys.argv[3])
+a = float(sys.argv[4])  # tasa de apredizaje
+maxEpocas = int(sys.argv[5])
 
 # Lectura de fichero
 f = open(fichero)
@@ -17,41 +58,80 @@ lineas_entrada = f.readlines()
 f.close()
 
 primera_linea = list(map(int, lineas_entrada[0].replace("\n", "").split(" ")))
-parametros = primera_linea[0]
+atributos = primera_linea[0]
 clases = primera_linea[1]
-print(clases)
 
-datos = np.empty((0, parametros + clases), float)
+datos = np.empty((0, atributos + 1), float)
 
 # for i in range lineas_entrada[0]
 
 for linea in lineas_entrada[1:]:
     linea_cortada = list(map(float, linea.replace("\n", "").split(" ")))
-    dato = linea_cortada[0:parametros]
+    dato = linea_cortada[0:atributos]
     # doy por hecho que siempre va a haber dos clases:
     if linea_cortada[-2] == 1:
         dato.append(1)
     else:
         dato.append(-1)
 
-    if linea_cortada[-1] == 1:
-        dato.append(1)
-    else:
-        dato.append(-1)
-
     datos = np.concatenate((datos, [dato]))
-print(datos)
 
-capa0 = np.array([NeuronaPX() for p in range(parametros)])
-print(capa0)
+
+if modo == 3:
+    # Lectura de fichero2
+    f2 = open(fichero2)
+    lineas_entrada2 = f2.readlines()
+    f2.close()
+
+    primera_linea2 = list(map(int, lineas_entrada2[0].replace("\n", "").split(" ")))
+    atributos2 = primera_linea2[0]
+    clases2 = primera_linea2[1]
+
+    datos2 = np.empty((0, atributos2 + 1), float)
+
+    for linea2 in lineas_entrada2[1:]:
+        linea_cortada2 = list(map(float, linea2.replace("\n", "").split(" ")))
+        dato2 = linea_cortada2[0:atributos2]
+
+        if linea_cortada2[-2] == 1:
+            dato2.append(1)
+        else:
+            dato2.append(-1)
+
+        datos2 = np.concatenate((datos2, [dato2]))
+
+# CREACION DE NEURONAS
+
+capa0 = np.array([NeuronaPX() for p in range(atributos)])
 capa1 = np.array([NeuronaPY(umbral)])
 
 for neurona in capa0:
     enlace = Enlace(0, neurona, capa1[0])
     neurona.addEnlaceSalida(enlace)
 
-# TODO: hay que cambiar esto por lo de FAA
-train, test = train_test_split(datos, test_size=1 - porcen_train)
+# train y test
+if modo == 1:  # en caso de que haya un porcentaje
+
+    nDatosTrain = math.ceil(datos.shape[0] * porcen_train)
+    indPerm = np.random.permutation(datos.shape[0])
+    train = datos[indPerm[: nDatosTrain]]
+    test = datos[indPerm[nDatosTrain:]]
+
+elif modo == 2:  # en caso de que no sea con porcentaje y queramos coger todo
+
+    train = datos
+    test = datos
+
+else:
+    train = datos
+    test = datos2
+
+print("TRAIN =================")
+print(train)
+print("TEST =================")
+print(test)
+
+antiguob = 0
 b = 0
 
 pesos = []
@@ -70,11 +150,32 @@ while True and contadorEpocas < maxEpocas:
                 neurona.enlaceSalida.cambiarPeso(neurona.enlaceSalida.peso + a * entrada[-1] * entrada[i])
             b = b + a * entrada[-1]
     for i, neurona in enumerate(capa0):
-        if neurona.enlaceSalida.peso - pesos[i]:
+        if abs(neurona.enlaceSalida.peso - pesos[i]) != 0:
             flag = True
-            pesos[i] = neurona.enlaceSalida.peso
-    print(b)
+        pesos[i] = neurona.enlaceSalida.peso
+    if abs(antiguob - b) != 0:
+        flag = True
+    antiguob = b
     # comparar bien los pesos (la bandera creo que no funciona o algo está mal porque no para)
     if not flag:
+        print("bieeeen")
         break
     contadorEpocas += 1
+
+print(pesos)
+print(b)
+
+# TEST
+fallos = 0
+for entrada in test:
+    for i, neurona in enumerate(capa0):
+        neurona.recibirSenal(entrada[i])
+    salida = capa1[0].funcionActivacion(b)
+
+    if salida != entrada[-1]:
+        fallos += 1
+
+tasaError = fallos / len(test) * 100
+
+print("Tasa Error: " + str(tasaError) + " %")
+
